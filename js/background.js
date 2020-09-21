40e03892f3db36050410519fc1c9c8ff28fee480
Tong Yang <tongyang@Tongs-MacBook-Air.local>
2020-09-21 14:59:24 +1000

    Site updated: 2020-09-21 14:59:23

--- js/background.js
+++ js/background.js
@@ -16,23 +16,7 @@ xiaokang.mobileSidebar();
 xiaokang.codeFull();
 xiaokang.bgPage();
 xiaokang.imgList = [
-  "https://ae01.alicdn.com/kf/H21b5f6b8496141a1979a33666e1074d9x.jpg",
-  "https://i.pximg.net/img-original/img/2015/12/10/16/28/17/53969178_p1.jpg",
-  "https://i.pximg.net/img-original/img/2015/12/10/16/28/17/53969178_p3.jpg",
-  "https://i.pximg.net/img-original/img/2015/12/10/16/28/17/53969178_p4.jpg",
-  "https://i.pximg.net/img-original/img/2015/12/10/16/28/17/53969178_p6.jpg",
-  "https://i.pximg.net/img-original/img/2015/12/10/16/28/17/53969178_p7.jpg",
-  "https://i.pximg.net/img-original/img/2015/12/10/16/28/17/53969178_p8.jpg",
-  "https://i.pximg.net/img-original/img/2015/12/10/16/28/17/53969178_p9.jpg",
-  "https://i.pximg.net/img-original/img/2015/12/10/16/28/17/53969178_p10.jpg",
-  "https://i.pximg.net/img-original/img/2020/05/31/00/03/14/81974290_p0.jpg",
-  "https://i.pximg.net/img-original/img/2020/05/10/00/03/14/81438900_p0.jpg",
-  "https://i.pximg.net/img-original/img/2019/06/10/00/03/26/75148936_p0.jpg",
-  "https://i.pximg.net/img-original/img/2019/05/13/12/10/01/74707810_p1.jpg",
-  "https://i.pximg.net/img-original/img/2019/05/20/12/25/16/74817763_p0.jpg",
-  "https://i.pximg.net/img-original/img/2019/05/20/12/25/16/74817763_p1.jpg",
-  
-];
+  "https://ae01.alicdn.com/kf/H21b5f6b8496141a1979a33666e1074d9x.jpg"];
 // 调用随机背景
 xiaokang.randomBg();
 
